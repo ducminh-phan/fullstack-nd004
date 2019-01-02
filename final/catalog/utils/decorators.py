ad2c8ce49d279611899d57ad6afeaa4110c0f614
Duc-Minh Phan <alephvn@gmail.com>
2019-01-02 21:21:45 +0700

    Add the decorator to require a logged in user,

--- final/catalog/utils/decorators.py
+++ final/catalog/utils/decorators.py
@@ -4,6 +4,11 @@ import werkzeug.exceptions
 from flask import request
 
 from catalog import errors
+from catalog.jwttoken import decode
+from catalog.models.user import User
+
+AUTH_HEADER_VALUE_PREFIX = "Bearer "
+INVALID_TOKEN_MESSAGE = "Access token is not valid"
 
 
 def parse_args_with(schema):
@@ -34,3 +39,38 @@ def parse_args_with(schema):
         return decorated_function
 
     return parse_args_with_decorator
+
+
+def require_logged_in(f):
+    """
+    Check if the user has logged in by checking the Authorization header,
+    and load the user into the argument `user` of the controller
+    """
+
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if "Authorization" not in request.headers:
+            raise errors.BadRequest()
+
+        access_token = request.headers["Authorization"][len(AUTH_HEADER_VALUE_PREFIX) :]
+
+        if access_token is None:
+            raise errors.BadRequest("Not logged in")
+
+        access_token_decoded = decode(access_token)
+
+        if access_token_decoded is None:
+            raise errors.Unauthorized(INVALID_TOKEN_MESSAGE)
+
+        user_id = access_token_decoded.get("user_id")
+        nonce = access_token_decoded.get("nonce")
+        user = User.get_by_id(user_id)
+
+        if user is None or user.access_token_nonce != nonce:
+            raise errors.Unauthorized(INVALID_TOKEN_MESSAGE)
+
+        kwargs["user"] = user
+
+        return f(*args, **kwargs)
+
+    return wrapper
