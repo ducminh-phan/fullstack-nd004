f9a87c890c7652fbe3be27d754c8376a80755a01
Duc-Minh Phan <alephvn@gmail.com>
2019-01-02 14:26:11 +0700

    Generate new nonce when a user login to prevent replay attack

--- final/catalog/controllers/auth.py
+++ final/catalog/controllers/auth.py
@@ -2,12 +2,23 @@ from werkzeug.security import check_password_hash
 
 from catalog import app
 from catalog.errors import BadRequest
-from catalog.jwttoken import access_token_schema, encode
+from catalog.extensions import db
+from catalog.jwttoken import access_token_schema, encode, generate_access_token_nonce
 from catalog.models import User
 from catalog.schemas import UserLoginSchema
 from catalog.utils.decorators import parse_args_with
 
 INVALID_LOGIN_MESSAGE = "Invalid user login, please re-check your login credentials."
+db_session = db.session
+
+
+def _change_user_nonce(user):
+    """Generate new nonce for the user"""
+    nonce = generate_access_token_nonce()
+    user.access_token_nonce = nonce
+
+    db_session.add(user)
+    db_session.commit()
 
 
 @app.route("/login", methods=("POST",))
@@ -21,5 +32,8 @@ def login(args):
     # Check if the user exists and the password is correct
     if user is None or not check_password_hash(user.password, password):
         raise BadRequest(INVALID_LOGIN_MESSAGE)
+
+    # Generate new nonce for the user to prevent replay attack
+    _change_user_nonce(user)
 
     return access_token_schema.jsonify({"access_token": encode(user)})
