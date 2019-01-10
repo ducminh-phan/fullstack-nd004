a027d5f1845a91086fbcf35024f980a4355d3835
Duc-Minh Phan <alephvn@gmail.com>
2019-01-10 11:33:08 +0700

    Fix folder structure

--- final-frontend/src/components/Flash/index.js
+++ final-frontend/src/components/Flash/index.js
@@ -1,6 +1,38 @@
+import React from 'react';
 import { connect } from 'react-redux';
+import { Alert, Collapse } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
-import Flash from './Flash';
+
+function typeToStyle(type) {
+  switch (type) {
+    case 'success':
+      return 'success';
+    case 'error':
+      return 'danger';
+    default:
+      return 'info';
+  }
+}
+
+
+const Flash = props => (
+  <div className="row">
+    <div className="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+      <Collapse in={!!props.message}>
+        <Alert bsStyle={typeToStyle(props.type)}>
+          {props.message}
+        </Alert>
+      </Collapse>
+    </div>
+  </div>
+);
+
+
+Flash.propTypes = {
+  type: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+};
 
 
 const mapStateToProps = ({ alert }) => ({ type: alert.type, message: alert.message });
--- final-frontend/src/components/Item/index.js
+++ final-frontend/src/components/Item/index.js
@@ -1,8 +1,63 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { Panel } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import ReactRouterPropTypes from 'react-router-prop-types';
 
 import { getItem } from '../../actions/item';
-import Item from './Item';
+import formWrapper from '../../utils/wrappers/formWrapper';
+
+
+class Item extends Component {
+  componentDidMount() {
+    const { categoryId, itemId } = this.props.match.params;
+    this.props.getItem(categoryId, itemId);
+  }
+
+  render() {
+    if (!this.props.item) {
+      return null;
+    }
+
+    return (
+      <Panel>
+        <Panel.Heading>
+          <Panel.Title>
+            {this.props.item.name}
+          </Panel.Title>
+        </Panel.Heading>
+
+        <Panel.Body>
+          {this.props.item.description}
+        </Panel.Body>
+
+        <Panel.Footer>
+          Category:
+          {' '}
+          {this.props.item.category.name}
+        </Panel.Footer>
+      </Panel>);
+  }
+}
+
+
+Item.propTypes = {
+  getItem: PropTypes.func.isRequired,
+  item: PropTypes.shape({
+    name: PropTypes.string,
+    description: PropTypes.string,
+    category: PropTypes.shape({
+      name: PropTypes.string,
+    }),
+  }),
+  match: ReactRouterPropTypes.match.isRequired,
+};
+
+
+Item.defaultProps = {
+  item: null,
+};
 
 
 const mapStateToProps = state => ({ item: state.item });
@@ -16,4 +71,4 @@ const mapDispatchToProps = dispatch => ({
 export default withRouter(connect(
   mapStateToProps,
   mapDispatchToProps,
-)(Item));
+)(formWrapper(Item)));
